7376321ae7fd8219f35600c15068ee97310a824d
Riley1101 <arkardev00@gmail.com>
2023-11-16 23:24:21 -0500

    constant added

--- main.c
+++ main.c
@@ -5,9 +5,13 @@
 int main(int argc, char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT);
+  writeChunk(&chunk, constant);
   writeChunk(&chunk, OP_RETURN);
-  // freeChunk(&chunk);
-  // disassembleChunk(&chunk, "test chunk");
   disassembleInstruction(&chunk, 0);
+  disassembleInstruction(&chunk, 1);
+  disassembleInstruction(&chunk, 2);
+  freeChunk(&chunk);
   return 0;
 }
