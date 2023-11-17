457097e2259b186dcd13fa05fdb8f8fd2ef9f4cf
Riley1101 <arkardev00@gmail.com>
2023-11-16 23:34:01 -0500

    update on line informations

--- main.c
+++ main.c
@@ -6,12 +6,10 @@ int main(int argc, char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT);
-  writeChunk(&chunk, constant);
-  writeChunk(&chunk, OP_RETURN);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, OP_RETURN, 123);
   disassembleInstruction(&chunk, 0);
   disassembleInstruction(&chunk, 1);
-  disassembleInstruction(&chunk, 2);
   freeChunk(&chunk);
   return 0;
 }
